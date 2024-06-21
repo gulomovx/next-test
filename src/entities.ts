214ed2f5a277e8771b17358792fedd1da6343771
gulomovx <xgulomov447@gmail.com>
2024-06-21 09:36:40 +0500

    error solved

--- src/entities.ts
+++ src/entities.ts
@@ -2,7 +2,7 @@ export type User = {
     
     id: number;
     name: string;
-    isAdmin: boolean;
+    isAdmin?: boolean;
     
 }
 export type Category = {
--- tests/components/UserAccount.test.tsx
+++ tests/components/UserAccount.test.tsx
@@ -1,7 +1,12 @@
-import { render, screen } from '@testing-library/react'
+import React from "react";
+import UserAccount from "../../src/app/components/UserAccount"; /// path ni @ bilan bersa xato beradi
+import { User } from "@/entities";
+import { render, screen } from "@testing-library/react";
 
-describe('UserAccount', () => {
-    it('should render user name', () => {
-        
-    })
-})
+describe("UserAccount", () => {
+  it("should render user name", () => {
+    const user: User = { id: 1, name: "developer" };
+    render(<UserAccount user={user} />);
+    expect(screen.getByText(user.name));
+  });
+});
